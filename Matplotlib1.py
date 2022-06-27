6de278f320b746d881ffec00a511371d29dd225c
GUILHERME FERNANDES NETO <40431403+gangagoura@users.noreply.github.com>
2022-06-27 13:58:10 -0300

    Create Matplotlib1.py

---
+++ Matplotlib1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Load data from a text file
+!wget https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/grades.csv
+df_students = pd.read_csv('grades.csv',delimiter=',',header='infer')
+
+# Remove any rows with missing data
+df_students = df_students.dropna(axis=0, how='any')
+
+# Calculate who passed, assuming '60' is the grade needed to pass
+passes  = pd.Series(df_students['Grade'] >= 60)
+
+# Save who passed to the Pandas dataframe
+df_students = pd.concat([df_students, passes.rename("Pass")], axis=1)
+
+
+# Print the result out into this notebook
+df_students 
